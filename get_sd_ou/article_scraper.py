dfbfbe6320336d2630c526f3aa2ad608fe7c6f3b
Erfan Ghofrani <parsyab1@gmail.com>
2021-05-11 10:19:50 +0430

    add argparse

--- get_sd_ou/article_scraper.py
+++ get_sd_ou/article_scraper.py
@@ -8,18 +8,24 @@ from get_sd_ou.classUtil import Article, Url
 from get_sd_ou.config import Config
 from get_sd_ou.databaseUtil import get_articles, init_db, insert_article_data
 
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "localhost", 8080)
-socket.socket = socks.socksocket
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--proxy', help='Do use proxy or not', default=Config.USE_PROXY, action=argparse.BooleanOptionalAction)
+parser.add_argument('--path', action='store', type=str, default=Config.ARTICLES_URL_PATH, help='path to article links file.')
+
+args = parser.parse_args()
+
+if args.proxy:
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "localhost", 8080)
+    socket.socket = socks.socksocket
 
 logger = logging.getLogger('mainLogger')
 logger.setLevel(Config.LOG_LEVEL)
 
 import sys
-articles_path = None
-if len(sys.argv) == 2:
-    articles_path = sys.argv[1]
+articles_path = args.path
 
-articles_path = articles_path or Config.ARTICLES_URL_PATH
 log_path = articles_path + '.failed_urls.txt'
 
 with open(articles_path) as art_file:
@@ -64,9 +70,6 @@ for i, url_line in enumerate(url_lines):
     article._soup = soup
 
     del url_obj, resp
-
-    
- 
 
     article_data = article.get_article_data()
     if not article_data is None:
